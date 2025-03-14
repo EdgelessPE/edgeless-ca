c324ff0842198136342352262ea553219b1f86d1
Cno <dsyourshy@qq.com>
2025-03-14 21:26:53 +0800

    feat: 使用通用的BaseResponse返回数据

--- backend/routers/auth.go
+++ backend/routers/auth.go
@@ -13,32 +13,27 @@ func RegisterAuthRoutes(r *gin.RouterGroup) {
 	r.POST("/register", func(c *gin.Context) {
 		var payload vo.RegisterPayload
 		if err := c.BindJSON(&payload); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, vo.BaseResponse[any]{Code: http.StatusBadRequest, Msg: err.Error(), Data: nil})
 			return
 		}
 
 		// 用户不能为空且长度不少于 7 个字符
 		if payload.Name == "" || len(payload.Name) < 7 {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "user name is required and must be at least 7 characters long"})
+			c.JSON(http.StatusBadRequest, vo.BaseResponse[any]{Code: http.StatusBadRequest, Msg: "用户名不能为空且长度不少于 7 个字符", Data: nil})
 			return
 		}
 
 		// 密码不能为空
 		if payload.PwdHash == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "password is required"})
+			c.JSON(http.StatusBadRequest, vo.BaseResponse[any]{Code: http.StatusBadRequest, Msg: "密码不能为空", Data: nil})
 			return
 		}
 
 		// 检查用户名和邮箱重复
 		var user models.User
-		config.DB.Where("name = ?", payload.Name).First(&user)
+		config.DB.Where("name = ? OR email = ?", payload.Name, payload.Email).First(&user)
 		if user.Name != "" {
-			c.JSON(http.StatusConflict, gin.H{"error": "user name already exists"})
-			return
-		}
-		config.DB.Where("email = ?", payload.Email).First(&user)
-		if user.Name != "" {
-			c.JSON(http.StatusConflict, gin.H{"error": "user email already exists"})
+			c.JSON(http.StatusConflict, vo.BaseResponse[any]{Code: http.StatusConflict, Msg: "用户名或邮箱已存在", Data: nil})
 			return
 		}
 
@@ -48,24 +43,27 @@ func RegisterAuthRoutes(r *gin.RouterGroup) {
 			PwdHash: payload.PwdHash,
 		}
 		config.DB.Create(&user)
-		c.JSON(http.StatusOK, gin.H{"user": user})
+		c.JSON(http.StatusOK, vo.BaseResponse[models.User]{Code: http.StatusOK, Msg: "Success", Data: user})
 	})
 	r.POST("/login", func(c *gin.Context) {
 		var payload vo.LoginPayload
 		if err := c.ShouldBind(&payload); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, vo.BaseResponse[any]{Code: http.StatusBadRequest, Msg: err.Error(), Data: nil})
+			return
 		}
 
 		var user models.User
 		// 查找用户，名称或邮箱匹配
 		config.DB.Where("name =? OR email =?", payload.Name, payload.Name).First(&user)
 		if user.Name == "" {
-			c.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
+			c.JSON(http.StatusNotFound, vo.BaseResponse[any]{Code: http.StatusNotFound, Msg: "用户未找到", Data: nil})
+			return
 		}
 		if payload.PwdHash != user.PwdHash {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "wrong password"})
+			c.JSON(http.StatusUnauthorized, vo.BaseResponse[any]{Code: http.StatusUnauthorized, Msg: "密码错误", Data: nil})
+			return
 		}
 		token, _ := config.GenerateToken(user.ID)
-		c.JSON(http.StatusOK, gin.H{"name": user.Name, "email": user.Email, "token": token})
+		c.JSON(http.StatusOK, vo.BaseResponse[map[string]string]{Code: http.StatusOK, Msg: "Success", Data: map[string]string{"name": user.Name, "email": user.Email, "token": token}})
 	})
 }
--- backend/routers/token.go
+++ backend/routers/token.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"nep-keychain-backend/config"
 	"nep-keychain-backend/models"
+	"nep-keychain-backend/vo"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,13 +24,13 @@ func NewTokenHandler(c *gin.Context) {
 	// 查找当前用户
 	result := config.DB.First(&user, c.GetUint("userID"))
 	if result.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "用户未找到"})
+		c.JSON(http.StatusNotFound, vo.BaseResponse[any]{Code: http.StatusNotFound, Msg: "用户未找到", Data: nil})
 		return
 	}
 
 	// 检查用户是否已经有密钥对
 	if user.PublicToken != "" {
-		c.JSON(http.StatusConflict, gin.H{"error": "用户已经有密钥对"})
+		c.JSON(http.StatusConflict, vo.BaseResponse[any]{Code: http.StatusConflict, Msg: "用户已经有密钥对", Data: nil})
 		return
 	}
 
@@ -41,9 +42,7 @@ func NewTokenHandler(c *gin.Context) {
 	user.PrivateToken = privateKey
 	config.DB.Save(&user)
 
-	c.JSON(http.StatusOK, gin.H{
-		"message": "密钥对生成成功",
-	})
+	c.JSON(http.StatusOK, vo.BaseResponse[string]{Code: http.StatusOK, Msg: "密钥对生成成功", Data: ""})
 }
 
 // GetPublicKeyHandler 获取用户公钥
@@ -54,13 +53,11 @@ func GetPublicKeyHandler(c *gin.Context) {
 	// 查找用户
 	result := config.DB.Where("name = ? OR email = ?", name, name).First(&user)
 	if result.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "用户未找到"})
+		c.JSON(http.StatusNotFound, vo.BaseResponse[any]{Code: http.StatusNotFound, Msg: "用户未找到", Data: nil})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"public_key": user.PublicToken,
-	})
+	c.JSON(http.StatusOK, vo.BaseResponse[string]{Code: http.StatusOK, Msg: "Success", Data: user.PublicToken})
 }
 
 // GetKeyPairHandler 获取当前用户的密钥对
@@ -70,14 +67,11 @@ func GetKeyPairHandler(c *gin.Context) {
 	// 查找当前用户
 	result := config.DB.First(&user, c.GetUint("userID"))
 	if result.Error != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "用户未找到"})
+		c.JSON(http.StatusNotFound, vo.BaseResponse[any]{Code: http.StatusNotFound, Msg: "用户未找到", Data: nil})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"public_key":  user.PublicToken,
-		"private_key": user.PrivateToken,
-	})
+	c.JSON(http.StatusOK, vo.BaseResponse[map[string]string]{Code: http.StatusOK, Msg: "Success", Data: map[string]string{"public_key": user.PublicToken, "private_key": user.PrivateToken}})
 }
 
 func RegisterTokenRoutes(r *gin.RouterGroup) {
