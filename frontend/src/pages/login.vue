d0642ba1dae0cc262d0b11a1a6d30690c54e8f03
Cno <dsyourshy@qq.com>
2025-03-15 23:22:21 +0800

    feat： 添加介绍文本

--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -1,12 +1,19 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-#134597 to-#338aca"
-  >
+  <div class="flex flex-col items-center justify-center h-screen bg-black">
     <div
       class="flex items-center justify-between h-lg w-80% bg-#1f1f1f rounded-3xl"
     >
-      <div class="flex-1 items-center justify-center flex items-center">
-        <div class="h-full text-2xl font-bold">Edgeless CA</div>
+      <div
+        class="flex-1 items-center justify-center flex flex-col items-center gap-4px"
+      >
+        <div
+          class="h-full text-36px line-height-48px font-bold bg-gradient-to-b from-#134597 to-#338aca bg-clip-text text-transparent"
+        >
+          Edgeless CA
+        </div>
+        <span class="text-16px line-height-24px text-#808080">
+          可信 Nep 包密钥对服务
+        </span>
       </div>
       <div class="w-40% h-full flex flex-col gap-4 rounded-3xl bg-#18181b p-12">
         <div>
