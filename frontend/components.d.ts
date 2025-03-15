3aab49579c96bf260fa6c27807944840081c71cf
Cno <dsyourshy@qq.com>
2025-03-15 22:03:20 +0800

    feat: 调整登录界面

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     Button: typeof import('primevue/button')['default']
     Divider: typeof import('primevue/divider')['default']
+    FloatLabel: typeof import('primevue/floatlabel')['default']
     InputText: typeof import('primevue/inputtext')['default']
     Password: typeof import('primevue/password')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -8,9 +8,7 @@
       <div class="flex-1 items-center justify-center flex items-center">
         <div class="h-full text-2xl font-bold">Edgeless CA</div>
       </div>
-      <div
-        class="w-40% h-full flex flex-col gap-4 rounded-3xl text-#000000ee bg-#eee p-12"
-      >
+      <div class="w-40% h-full flex flex-col gap-4 rounded-3xl bg-#18181b p-12">
         <div>
           <h2 class="text-2xl font-bold">登录</h2>
           <span>使用 GitHub 注册或登录</span>
@@ -23,14 +21,20 @@
             :size="showEmailLogin ? undefined : 'large'"
           />
         </div>
-        <Divider align="center" type="dotted">
+        <Divider align="center" type="dotted" class="!mb-0">
           <b>或</b>
         </Divider>
         <template v-if="showEmailLogin">
           <span>使用邮箱登录</span>
           <div class="flex flex-col gap-2 w-full">
-            <InputText name="email" type="text" placeholder="Email" fluid />
-            <Password name="password" placeholder="Password" toggleMask fluid />
+            <FloatLabel variant="on">
+              <InputText name="email" type="text" fluid />
+              <label for="on_label">邮箱</label>
+            </FloatLabel>
+            <FloatLabel variant="on">
+              <Password name="password" toggleMask fluid />
+              <label for="on_label">密码</label>
+            </FloatLabel>
           </div>
           <div style="height: 32px">
             <Button class="w-full" label="登录" />
@@ -46,9 +50,7 @@
             @click="showEmailLogin = true"
           />
         </template>
-        <div class="text-12px text-#00000088 cursor-pointer mt-2" href="#">
-          忘记密码？
-        </div>
+        <div class="text-12px cursor-pointer mt-2" href="#">忘记密码？</div>
       </div>
     </div>
   </div>
